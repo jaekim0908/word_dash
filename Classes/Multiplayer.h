10fa7b32268b8104c33e2fea6523aac6803e0d6e
Jae Kim <jae.kim0908@gmail.com>
2012-01-05 23:48:45 -0800

    Adding multi-player game mode with game center integration.

---
+++ Classes/Multiplayer.h
@@ -0,0 +1,13 @@
+//
+//  Multiplayer.h
+//  HundredSeconds
+//
+//  Created by Jae Kim on 12/12/11.
+//  Copyright (c) 2011 experiencesquad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Multiplayer : NSObject
+
+@end
---
+++ Classes/Multiplayer.m
@@ -0,0 +1,13 @@
+//
+//  Multiplayer.m
+//  HundredSeconds
+//
+//  Created by Jae Kim on 12/12/11.
+//  Copyright (c) 2011 experiencesquad. All rights reserved.
+//
+
+#import "Multiplayer.h"
+
+@implementation Multiplayer
+
+@end
