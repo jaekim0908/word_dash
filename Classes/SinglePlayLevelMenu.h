c11b745ed12d98f50008c64830c8ec3f2bdeca2d
Michael Ho <mikecho@yahoo.com>
2012-01-27 01:27:23 -0800

    Added new graphics for single player levels menu

--- Classes/SinglePlayLevelMenu.h
+++ Classes/SinglePlayLevelMenu.h
@@ -11,8 +11,8 @@
 #import "Constants.h"
 
 @interface SinglePlayLevelMenu : CCLayer{
-    CCSprite *sandBackground;
-    CCSprite *levelContainer;
+    CCSprite *beachBackground;
+
     
     CCSprite *level1Button;
     CCSprite *level2Button;
@@ -20,45 +20,43 @@
     CCSprite *level4Button;
     CCSprite *level5Button;
     
+    CCSprite *backButton;
+    
+    BOOL level1Locked;
+    BOOL level2Locked;
+    BOOL level3Locked;
+    BOOL level4Locked;
+    BOOL level5Locked;
+    
     CCLabelTTF *level1Literal;
     CCLabelTTF *level2Literal;
     CCLabelTTF *level3Literal;
     CCLabelTTF *level4Literal;
     CCLabelTTF *level5Literal;
     
-    CCSprite *level1Background;
     CCSprite *level1BeatAIAward;
     CCSprite *level1TotalPointsAward;
     CCSprite *level1LongWordAward;    
 
-    CCSprite *level2Background;
-    CCSprite *level2Lock;
     CCSprite *level2BeatAIAward;
     CCSprite *level2TotalPointsAward;
     CCSprite *level2LongWordAward;
 
-    CCSprite *level3Background;
-    CCSprite *level3Lock;
     CCSprite *level3BeatAIAward;
     CCSprite *level3TotalPointsAward;
     CCSprite *level3LongWordAward;
 
-    CCSprite *level4Background;
-    CCSprite *level4Lock;
     CCSprite *level4BeatAIAward;
     CCSprite *level4TotalPointsAward;
     CCSprite *level4LongWordAward;
 
-    CCSprite *level5Background;
-    CCSprite *level5Lock;
     CCSprite *level5BeatAIAward;
     CCSprite *level5TotalPointsAward;
     CCSprite *level5LongWordAward;
 
 }
 
-@property (nonatomic, retain) CCSprite *sandBackground;
-@property (nonatomic, retain) CCSprite *levelContainer;
+@property (nonatomic, retain) CCSprite *beachBackground;
 
 @property (nonatomic, retain) CCSprite *level1Button;
 @property (nonatomic, retain) CCSprite *level2Button;
@@ -66,37 +64,33 @@
 @property (nonatomic, retain) CCSprite *level4Button;
 @property (nonatomic, retain) CCSprite *level5Button;
 
+@property (nonatomic, retain) CCSprite *backButton;
+
 @property (nonatomic, retain) CCLabelTTF *level1Literal;
 @property (nonatomic, retain) CCLabelTTF *level2Literal;
 @property (nonatomic, retain) CCLabelTTF *level3Literal;
 @property (nonatomic, retain) CCLabelTTF *level4Literal;
 @property (nonatomic, retain) CCLabelTTF *level5Literal;
 
-@property (nonatomic, retain) CCSprite *level1Background;
 @property (nonatomic, retain) CCSprite *level1BeatAIAward;
 @property (nonatomic, retain) CCSprite *level1TotalPointsAward;
 @property (nonatomic, retain) CCSprite *level1LongWordAward;
 
-@property (nonatomic, retain) CCSprite *level2Background;
-@property (nonatomic, retain) CCSprite *level2Lock;
+
 @property (nonatomic, retain) CCSprite *level2BeatAIAward;
 @property (nonatomic, retain) CCSprite *level2TotalPointsAward;
 @property (nonatomic, retain) CCSprite *level2LongWordAward;
 
-@property (nonatomic, retain) CCSprite *level3Background;
-@property (nonatomic, retain) CCSprite *level3Lock;
+
 @property (nonatomic, retain) CCSprite *level3BeatAIAward;
 @property (nonatomic, retain) CCSprite *level3TotalPointsAward;
 @property (nonatomic, retain) CCSprite *level3LongWordAward;
 
-@property (nonatomic, retain) CCSprite *level4Background;
-@property (nonatomic, retain) CCSprite *level4Lock;
 @property (nonatomic, retain) CCSprite *level4BeatAIAward;
 @property (nonatomic, retain) CCSprite *level4TotalPointsAward;
 @property (nonatomic, retain) CCSprite *level4LongWordAward;
 
-@property (nonatomic, retain) CCSprite *level5Background;
-@property (nonatomic, retain) CCSprite *level5Lock;
+
 @property (nonatomic, retain) CCSprite *level5BeatAIAward;
 @property (nonatomic, retain) CCSprite *level5TotalPointsAward;
 @property (nonatomic, retain) CCSprite *level5LongWordAward;
--- Classes/SinglePlayLevelMenu.mm
+++ Classes/SinglePlayLevelMenu.mm
@@ -11,8 +11,7 @@
 
 @implementation SinglePlayLevelMenu
 
-@synthesize sandBackground;
-@synthesize levelContainer;
+@synthesize beachBackground;
 
 @synthesize level1Button;
 @synthesize level2Button;
@@ -20,37 +19,24 @@
 @synthesize level4Button;
 @synthesize level5Button;
 
-@synthesize level1Literal;
-@synthesize level2Literal;
-@synthesize level3Literal;
-@synthesize level4Literal;
-@synthesize level5Literal;
+@synthesize backButton;
 
-@synthesize level1Background;
 @synthesize level1BeatAIAward;
 @synthesize level1TotalPointsAward;
 @synthesize level1LongWordAward;
 
-@synthesize level2Background;
-@synthesize level2Lock;
 @synthesize level2BeatAIAward;
 @synthesize level2TotalPointsAward;
 @synthesize level2LongWordAward;
 
-@synthesize level3Background;
-@synthesize level3Lock;
 @synthesize level3BeatAIAward;
 @synthesize level3TotalPointsAward;
 @synthesize level3LongWordAward;
 
-@synthesize level4Background;
-@synthesize level4Lock;
 @synthesize level4BeatAIAward;
 @synthesize level4TotalPointsAward;
 @synthesize level4LongWordAward;
 
-@synthesize level5Background;
-@synthesize level5Lock;
 @synthesize level5BeatAIAward;
 @synthesize level5TotalPointsAward;
 @synthesize level5LongWordAward;
@@ -83,205 +69,124 @@
         
         self.isTouchEnabled = YES;
         
-        //sandBackground = [CCSprite spriteWithFile:@"black-background.png"];
-        //sandBackground = [CCSprite spriteWithFile:@"footPrintWhiteSand.png"];
-        sandBackground = [CCSprite spriteWithFile:@"whiteSandBg.png"];
-        sandBackground.position = ccp(240, 160);
-        [self addChild:sandBackground z:0];
+        beachBackground = [CCSprite spriteWithFile:@"Level-Background_004.png"];
+        beachBackground.position = ccp(240, 160);
+        [self addChild:beachBackground z:0];
 
-        levelContainer = [CCSprite spriteWithFile:@"level_superbackground.png"];
-        levelContainer.position = ccp(240, 228);
-        [self addChild:levelContainer z:1];
 
-        
         //LEVEL ONE
-        
-        level1Background = [CCSprite spriteWithFile:@"level_background.png"];
-        level1Background.position = ccp(65, 228);
-        //level1Background.position = ccp(40, 228);
-        [self addChild:level1Background z:1];
-
-        level1Button = [CCSprite spriteWithFile:@"blueSandDollar.png"];
-        level1Button.position = ccp(39+25, 243);
+        level1Button = [CCSprite spriteWithFile:@"Level_001.png"];
+        level1Button.position = ccp(39+25, 160);
         [self addChild:level1Button z:2];
         
-        level1Literal = [CCLabelTTF labelWithString:@"1"
-                                               fontName:@"Marker Felt" 
-                                               fontSize:32]; 
-		level1Literal.color = ccc3(255,255,255);;
-		level1Literal.position = ccp(40+25,240);
-        [self addChild:level1Literal z:3];
-        
-
-        level1BeatAIAward = [CCSprite spriteWithFile:@"trophy.png"];
+        level1BeatAIAward = [CCSprite spriteWithFile:@"Trophy001.png"];
         //level1BeatAIAward.position = ccp(10, 200);
-        level1BeatAIAward.position = ccp(13+25, 200);
+        level1BeatAIAward.position = ccp(64, 160);
         level1BeatAIAward.visible=NO;
         [self addChild:level1BeatAIAward z:2];
         
-        level1TotalPointsAward = [CCSprite spriteWithFile:@"coins.png"];
-        level1TotalPointsAward.position = ccp(35+25, 200);
+        level1TotalPointsAward = [CCSprite spriteWithFile:@"Coins001.png"];
+        level1TotalPointsAward.position = ccp(64, 100);
         level1TotalPointsAward.visible=NO;
         [self addChild:level1TotalPointsAward z:2];
         
-        level1LongWordAward = [CCSprite spriteWithFile:@"dictionary.png"];
-        level1LongWordAward.position = ccp(62+25, 200);
+        level1LongWordAward = [CCSprite spriteWithFile:@"Book002.png"];
+        level1LongWordAward.position = ccp(64, 50);
         level1LongWordAward.visible=NO;
         [self addChild:level1LongWordAward z:2];
 
         //LEVEL TWO
-        
-        level2Background = [CCSprite spriteWithFile:@"level_background.png"];
-        //level2Background.position = ccp(140, 228);
-        level2Background.position = ccp(152, 228);
-        [self addChild:level2Background z:1];
-
-        level2Button = [CCSprite spriteWithFile:@"blueSandDollar.png"];
+        level2Button = [CCSprite spriteWithFile:@"Level_002.png"];
         //level2Button.position = ccp(140, 240);
-        level2Button.position = ccp(139+12, 243);
+        level2Button.position = ccp(151, 160);
         [self addChild:level2Button z:2];
         
-        level2Literal = [CCLabelTTF labelWithString:@"2"
-                                           fontName:@"Marker Felt" 
-                                           fontSize:32]; 
-		level2Literal.color = ccc3(255,255,255);;
-		level2Literal.position = ccp(140+12,240);
-        [self addChild:level2Literal z:3];
-
-        level2Lock = [CCSprite spriteWithFile:@"lock.png"];
-        level2Lock.position = ccp(140+12, 250);
-        level2Lock.visible=YES;
-        [self addChild:level2Lock z:4];
-
-        level2BeatAIAward = [CCSprite spriteWithFile:@"trophy.png"];
-        level2BeatAIAward.position = ccp(113+12, 200);
+        level2BeatAIAward = [CCSprite spriteWithFile:@"Trophy001.png"];
+        level2BeatAIAward.position = ccp(151, 160);
         level2BeatAIAward.visible=NO;
         [self addChild:level2BeatAIAward z:1];
         
-        level2TotalPointsAward = [CCSprite spriteWithFile:@"coins.png"];
-        level2TotalPointsAward.position = ccp(135+12, 200);
+        level2TotalPointsAward = [CCSprite spriteWithFile:@"Coins001.png"];
+        level2TotalPointsAward.position = ccp(151, 100);
         level2TotalPointsAward.visible=NO;
         [self addChild:level2TotalPointsAward z:1];
         
-        level2LongWordAward = [CCSprite spriteWithFile:@"dictionary.png"];
-        level2LongWordAward.position = ccp(162+12, 200);
+        level2LongWordAward = [CCSprite spriteWithFile:@"Book002.png"];
+        level2LongWordAward.position = ccp(151, 50);
         level2LongWordAward.visible=NO;
         [self addChild:level2LongWordAward z:1];
 
         
         //LEVEL THREE
-        level3Background = [CCSprite spriteWithFile:@"level_background.png"];
-        //level3Background.position = ccp(240, 228);
-        level3Background.position = ccp(239, 228);
-        [self addChild:level3Background z:1];
 
-        level3Button = [CCSprite spriteWithFile:@"blueSandDollar.png"];
+        level3Button = [CCSprite spriteWithFile:@"Level_003.png"];
         //level3Button.position = ccp(240, 240);
-        level3Button.position = ccp(239, 243);
+        level3Button.position = ccp(239, 160);
         [self addChild:level3Button z:2];
-
-        level3Literal = [CCLabelTTF labelWithString:@"3"
-                                           fontName:@"Marker Felt" 
-                                           fontSize:32]; 
-		level3Literal.color = ccc3(255,255,255);;
-		level3Literal.position = ccp(240,240);
-        [self addChild:level3Literal z:3];
         
-        level3Lock = [CCSprite spriteWithFile:@"lock.png"];
-        level3Lock.position = ccp(240, 250);
-        level3Lock.visible=YES;
-        [self addChild:level3Lock z:4];
-
-        level3BeatAIAward = [CCSprite spriteWithFile:@"trophy.png"];
-        level3BeatAIAward.position = ccp(213, 200);
+        level3BeatAIAward = [CCSprite spriteWithFile:@"Trophy001.png"];
+        level3BeatAIAward.position = ccp(239, 160);
         level3BeatAIAward.visible=NO;
         [self addChild:level3BeatAIAward z:1];
         
-        level3TotalPointsAward = [CCSprite spriteWithFile:@"coins.png"];
-        level3TotalPointsAward.position = ccp(235, 200);
+        level3TotalPointsAward = [CCSprite spriteWithFile:@"Coins001.png"];
+        level3TotalPointsAward.position = ccp(239, 100);
         level3TotalPointsAward.visible=NO;
         [self addChild:level3TotalPointsAward z:1];
         
-        level3LongWordAward = [CCSprite spriteWithFile:@"dictionary.png"];
-        level3LongWordAward.position = ccp(262, 200);
+        level3LongWordAward = [CCSprite spriteWithFile:@"Book002.png"];
+        level3LongWordAward.position = ccp(239, 50);
         level3LongWordAward.visible=NO;
         [self addChild:level3LongWordAward z:1];
         
         //LEVEL FOUR
-        level4Background = [CCSprite spriteWithFile:@"level_background.png"];
-        //level4Background.position = ccp(340, 228);
-        level4Background.position = ccp(326, 228);
-        [self addChild:level4Background z:1];
 
-        level4Button = [CCSprite spriteWithFile:@"blueSandDollar.png"];
+        level4Button = [CCSprite spriteWithFile:@"Level_004.png"];
         //level4Button.position = ccp(340, 240);
-        level4Button.position = ccp(339-14, 243);
+        level4Button.position = ccp(339-14, 160);
         [self addChild:level4Button z:2];
 
-        level4Literal = [CCLabelTTF labelWithString:@"4"
-                                           fontName:@"Marker Felt" 
-                                           fontSize:32]; 
-		level4Literal.color = ccc3(255,255,255);;
-		level4Literal.position = ccp(340-14,240);
-        [self addChild:level4Literal z:3];
-
-        level4Lock = [CCSprite spriteWithFile:@"lock.png"];
-        level4Lock.position = ccp(340-14, 250);
-        level4Lock.visible=YES;
-        [self addChild:level4Lock z:4];
-
-        level4BeatAIAward = [CCSprite spriteWithFile:@"trophy.png"];
-        level4BeatAIAward.position = ccp(313-14, 200);
+        level4BeatAIAward = [CCSprite spriteWithFile:@"Trophy001.png"];
+        level4BeatAIAward.position = ccp(325, 160);
         level4BeatAIAward.visible=NO;
         [self addChild:level4BeatAIAward z:1];
         
-        level4TotalPointsAward = [CCSprite spriteWithFile:@"coins.png"];
-        level4TotalPointsAward.position = ccp(335-14, 200);
+        level4TotalPointsAward = [CCSprite spriteWithFile:@"Coins001.png"];
+        level4TotalPointsAward.position = ccp(325, 100);
         level4TotalPointsAward.visible=NO;
         [self addChild:level4TotalPointsAward z:1];
         
-        level4LongWordAward = [CCSprite spriteWithFile:@"dictionary.png"];
-        level4LongWordAward.position = ccp(362-14, 200);
+        level4LongWordAward = [CCSprite spriteWithFile:@"Book002.png"];
+        level4LongWordAward.position = ccp(325, 50);
         level4LongWordAward.visible=NO;
         [self addChild:level4LongWordAward z:1];
         
         //LEVEL FIVE
-        level5Background = [CCSprite spriteWithFile:@"level_background.png"];
-        //level5Background.position = ccp(440, 228);
-        level5Background.position = ccp(413, 228);
-        [self addChild:level5Background z:1];
-        
-        level5Button = [CCSprite spriteWithFile:@"blueSandDollar.png"];
+        level5Button = [CCSprite spriteWithFile:@"Level_005.png"];
         //level5Button.position = ccp(440, 240);
-        level5Button.position = ccp(439-27, 243);
+        level5Button.position = ccp(439-27, 160);
         [self addChild:level5Button z:2];
         
-        level5Literal = [CCLabelTTF labelWithString:@"5"
-                                           fontName:@"Marker Felt" 
-                                           fontSize:32]; 
-		level5Literal.color = ccc3(255,255,255);;
-		level5Literal.position = ccp(440-27,240);
-        [self addChild:level5Literal z:3];
-
-        level5Lock = [CCSprite spriteWithFile:@"lock.png"];
-        level5Lock.position = ccp(440-27, 250);
-        level5Lock.visible=YES;
-        [self addChild:level5Lock z:4];
-        
-        level5BeatAIAward = [CCSprite spriteWithFile:@"trophy.png"];
-        level5BeatAIAward.position = ccp(413-27, 200);
+        level5BeatAIAward = [CCSprite spriteWithFile:@"Trophy001.png"];
+        level5BeatAIAward.position = ccp(412, 160);
         level5BeatAIAward.visible=NO;
         [self addChild:level5BeatAIAward z:1];
         
-        level5TotalPointsAward = [CCSprite spriteWithFile:@"coins.png"];
-        level5TotalPointsAward.position = ccp(435-27, 200);
+        level5TotalPointsAward = [CCSprite spriteWithFile:@"Coins001.png"];
+        level5TotalPointsAward.position = ccp(412, 100);
         level5TotalPointsAward.visible=NO;
         [self addChild:level5TotalPointsAward z:1];
         
-        level5LongWordAward = [CCSprite spriteWithFile:@"dictionary.png"];
-        level5LongWordAward.position = ccp(462-27, 200);
+        level5LongWordAward = [CCSprite spriteWithFile:@"Book002.png"];
+        level5LongWordAward.position = ccp(412, 50);
         level5LongWordAward.visible=NO;
         [self addChild:level5LongWordAward z:1];
+        
+        //Back Button
+        backButton = [CCSprite spriteWithFile:@"Button_Back001.png"];
+        backButton.position = ccp(450, 30);
+        backButton.visible = NO;
+        [self addChild:backButton z:2];
         
         //DISPLAY THE STAR LEVELS BASED ON THE VALUES IN THE PLIST
         [self displayStars:@"Level1" 
@@ -291,25 +196,25 @@
                 LongWordAwardSprite:level1LongWordAward];
         
         [self displayStars:@"Level2" 
-                lockSprite:level2Lock
+                lockSprite:level2Button
                 BeatAIAwardSprite:level2BeatAIAward 
                 TotalPointAwardSprite:level2TotalPointsAward 
                 LongWordAwardSprite:level2LongWordAward];
 
         [self displayStars:@"Level3"
-                lockSprite:level3Lock
+                lockSprite:level3Button
                 BeatAIAwardSprite:level3BeatAIAward 
                 TotalPointAwardSprite:level3TotalPointsAward 
                 LongWordAwardSprite:level3LongWordAward];
 
         [self displayStars:@"Level4"
-                lockSprite:level4Lock
+                lockSprite:level4Button
                 BeatAIAwardSprite:level4BeatAIAward 
                 TotalPointAwardSprite:level4TotalPointsAward 
                 LongWordAwardSprite:level4LongWordAward];
 
         [self displayStars:@"Level5"
-                lockSprite:level5Lock
+                lockSprite:level5Button
                 BeatAIAwardSprite:level5BeatAIAward 
                 TotalPointAwardSprite:level5TotalPointsAward 
                 LongWordAwardSprite:level5LongWordAward];
@@ -327,22 +232,35 @@
     //NSMutableDictionary *prevLevelInfo;
     
     NSMutableDictionary *levelInfo = [ [[GameManager sharedGameManager] getGameLevelDictionary] objectForKey:levelName];
-    
-    //if (prevLevelName){
-    //    prevLevelInfo = [ [[GameManager sharedGameManager] getGameLevelDictionary] objectForKey:prevLevelName];
+
     
     if (lockSprite) {
-        [[levelInfo objectForKey:@"levelLocked"] boolValue] ? lockSprite.visible = YES : lockSprite.visible = NO;
+        if ([[levelInfo objectForKey:@"levelLocked"] boolValue]) {
+            
+            [lockSprite setOpacity:128];
+            
+            if([levelName isEqualToString:@"Level2"]) {
+                level2Locked = TRUE;
+            }
+            else if([levelName isEqualToString:@"Level3"]) {
+                level3Locked = TRUE;
+            }
+            else if([levelName isEqualToString:@"Level4"]) {
+                level4Locked = TRUE;
+            }
+            else if([levelName isEqualToString:@"Level5"]) {
+                level5Locked = TRUE;
+            }
+            
+        }
+        else{
+            [lockSprite setOpacity:255];
+        }
     }
-    //}
     
     beatAIAwardSprite.visible = [[levelInfo objectForKey:@"beatAIAward"] boolValue];
     totalPointAwardSprite.visible = [[levelInfo objectForKey:@"totalPointsAward"] boolValue];
     longWordAwardSprite.visible = [[levelInfo objectForKey:@"longWordAward"] boolValue];
-
-    //beatAIAwardSprite.visible = YES;
-    //totalPointAwardSprite.visible = YES;
-    //longWordAwardSprite.visible = YES;
 
 
 }
@@ -374,7 +292,7 @@
         [ [GameManager sharedGameManager] runLoadingSceneWithTargetId:kSinglePlayerScene];
     }
     else if(CGRectContainsPoint(level2Button.boundingBox,
-                                touchLocation) && !level2Lock.visible){
+                                touchLocation) && !level2Locked){
         CCLOG(@"Level 2 Button PRESSED.");
         
         NSMutableDictionary *levelInfo = [ [[GameManager sharedGameManager] getGameLevelDictionary] objectForKey:@"Level2"];
@@ -387,7 +305,7 @@
         [ [GameManager sharedGameManager] runLoadingSceneWithTargetId:kSinglePlayerScene];
     }
     else if(CGRectContainsPoint(level3Button.boundingBox,
-                                touchLocation) && !level3Lock.visible){
+                                touchLocation) && !level3Locked){
         CCLOG(@"Level 3 Button PRESSED.");
         
         NSMutableDictionary *levelInfo = [ [[GameManager sharedGameManager] getGameLevelDictionary] objectForKey:@"Level3"];
@@ -400,7 +318,7 @@
         [ [GameManager sharedGameManager] runLoadingSceneWithTargetId:kSinglePlayerScene];
     }
     else if(CGRectContainsPoint(level4Button.boundingBox,
-                                touchLocation) && !level4Lock.visible){
+                                touchLocation) && !level4Locked){
         CCLOG(@"Level 4 Button PRESSED.");
         
         NSMutableDictionary *levelInfo = [ [[GameManager sharedGameManager] getGameLevelDictionary] objectForKey:@"Level4"];
@@ -413,7 +331,7 @@
         [ [GameManager sharedGameManager] runLoadingSceneWithTargetId:kSinglePlayerScene];
     }
     else if(CGRectContainsPoint(level5Button.boundingBox,
-                                touchLocation) && !level5Lock.visible){
+                                touchLocation) && !level5Locked){
         CCLOG(@"Level 5 Button PRESSED.");
         
         NSMutableDictionary *levelInfo = [ [[GameManager sharedGameManager] getGameLevelDictionary] objectForKey:@"Level5"];
